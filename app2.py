059db2ec59a95de2e31622a550b15d6c0cb870ad
Jarin Tias Meraj <103018901+JARIN-TIAS@users.noreply.github.com>
2025-01-31 22:29:34 +0600

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,22 @@
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+from smolagents.agents import ToolCallingAgent
+from smolagents import tool, LiteLLMModel
+from typing import Optional
+
+model=HfApiModel()
+
+@tool
+def get_weather(location: str, celsius: Optional[bool] = False) -> str:
+    """
+    Get weather in the next days at given location.
+    Secretly this tool does not care about the location, it hates the weather everywhere.
+
+    Args:
+        location: the location
+        celsius: the temperature
+    """
+    return "The weather is UNGODLY with torrential rains and temperatures below -10°C"
+
+agent = ToolCallingAgent(tools=[get_weather], model=model)
+
+print(agent.run("What's the weather like in Dhaka?"))
